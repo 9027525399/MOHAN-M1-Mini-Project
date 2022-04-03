f0eb5a3bf57c611e2340976efa9b1478b30aeb8a
mohan <95026320+9027525399@users.noreply.github.com>
2022-04-03 16:27:16 +0530

    Add files via upload

---
+++ 3_Implementation/inc/calculator.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+ 
+void add(int num1,int num2);
+void sub(int num1,int num2);
+void mult(int num1,int num2);
+void divi(int num1,int num2);
+void modu(int num1,int num2);
+void fact(int num1);
+void power(int num1);
+void square(int num1);
+void cube(int num1);
+void squareroot(int num1);
+
+#endif
